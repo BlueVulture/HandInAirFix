fefcaf6ac7759ff3925b3b899538b8206fc53391
RattleTotem <domen.ramsak@gmail.com>
2021-05-21 20:54:48 +0200

    Add project files.

---
+++ HandInAirFix/AirHand.cs
@@ -0,0 +1,23 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.View.Animation;
+using Kingmaker.View.Equipment;
+
+namespace HandInAirFix
+{
+    [HarmonyLib.HarmonyPatch(typeof(UnitViewHandsEquipment), "get_ActiveOffHandWeaponStyle")]
+    class UnitViewHandsEquipment_ActiveOffHandWeaponStyle_Patch
+    {
+        static bool Prepare()
+        {
+            return Main.noHandInTheAir;
+        }
+
+        static void Postfix(UnitViewHandsEquipment __instance, ref WeaponAnimationStyle __result)
+        {
+            if (__instance.ActiveMainHandWeaponStyle != WeaponAnimationStyle.Fist && __result == WeaponAnimationStyle.Fist)
+            {
+                __result = WeaponAnimationStyle.None;
+            }
+        }
+    }
+}
---
+++ HandInAirFix/Main.cs
@@ -0,0 +1,41 @@
+﻿using UnityModManagerNet;
+using System;
+using System.Reflection;
+using Kingmaker.Blueprints;
+using System.Collections.Generic;
+
+namespace HandInAirFix
+{
+    internal class Main
+    {
+        public static bool noHandInTheAir = true;
+
+        internal static UnityModManagerNet.UnityModManager.ModEntry.ModLogger logger;
+        internal static HarmonyLib.Harmony harmony;
+        internal static LibraryScriptableObject library;
+
+        internal static void DebugError(Exception ex)
+        {
+            if (logger != null) logger.Log(ex.ToString() + "\n" + ex.StackTrace);
+        }
+        internal static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+
+                harmony = new HarmonyLib.Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                DebugError(ex);
+                throw ex;
+            }
+            return true;
+        }
+
+    }
+}
